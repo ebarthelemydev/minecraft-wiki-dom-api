8e886731dd1a77f68b4fb0a1289b825460622a18
ebarthelemydev <ebarthelemy@edenschool.fr>
2026-05-13 11:57:52 +0200

    script

--- script.js
+++ script.js
@@ -33,14 +33,23 @@ $form.addEventListener("submit", async (e) => {
 		img.classList.add("img-search");
 		divcard.appendChild(img);
 
+		let divsubtext = document.createElement("div");
+		divsubtext.classList.add("div-sub-text");
+		divcard.appendChild(divsubtext);
+
 		let divtext = document.createElement("div");
 		divtext.classList.add("div-text");
-		divcard.appendChild(divtext);
+		divsubtext.appendChild(divtext);
 
 		let a = document.createElement("a");
 		a.textContent = "Animal";
-		a.classList.add("ptype");
+		a.classList.add("a-card");
+		a.setAttribute("href", "");
 		divtext.appendChild(a);
+
+		let hr = document.createElement("hr");
+		hr.classList.add("hr-card");
+		divsubtext.appendChild(hr);
 
 		let ptype = document.createElement("p");
 		ptype.textContent = data[i].type;
@@ -55,16 +64,22 @@ $form.addEventListener("submit", async (e) => {
 		if (type == "neutral") {
 			p.style.backgroundColor = "rgba(227, 181, 153, 1)";
 			btn.style.backgroundColor = "rgba(227, 181, 153, 1)";
+			hr.style.backgroundColor = "rgba(227, 181, 153, 1)";
+			divcard.style.border = "solid 2px rgba(227, 181, 153, 1)";
 		}
 
 		if (type == "passive") {
 			p.style.backgroundColor = "rgba(79, 140, 105, 1)";
 			btn.style.backgroundColor = "rgba(79, 140, 105, 1)";
+			hr.style.backgroundColor = "rgba(79, 140, 105, 1)";
+			divcard.style.border = "solid 2px rgba(79, 140, 105, 1)";
 		}
 
 		if (type == "hostile") {
 			p.style.backgroundColor = "rgba(210, 70, 70, 1)";
 			btn.style.backgroundColor = "rgba(210, 70, 70, 1)";
+			hr.style.backgroundColor = "rgba(210, 70, 70, 1)";
+			divcard.style.border = "solid 2px rgba(210, 70, 70, 1)";
 		}
 	}
 });
