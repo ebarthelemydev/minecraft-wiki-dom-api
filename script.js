d8e9289d2c90ea059ef08b82a2578790a0e52540
ebarthelemydev <ebarthelemy@edenschool.fr>
2026-05-13 12:48:14 +0200

    main

--- script.js
+++ script.js
@@ -1,14 +1,66 @@
 const $btnsearch = document.querySelector(".btn-search");
 const $form = document.querySelector("form");
 const maincard = document.querySelector(".main-card");
+const inputHealth = document.querySelector("#health");
+const inputDamage = document.querySelector("#damage");
+const inputArmor = document.querySelector("#armor");
+const inputSearch = document.querySelector("#search");
+const selectClassification = document.querySelector("#classification");
+const selectType = document.querySelector("#type");
 
 $form.addEventListener("submit", async (e) => {
 	const searchvalue = $form.querySelector("input").value;
 
 	e.preventDefault();
 
-	const response = await fetch("http://10.69.4.208:3000/v1/entities/");
+	let isQueryAtLeastOne = false;
+	let url = "http://10.69.4.208:3000/v1/entities?";
+
+	if (inputHealth.value != "") {
+		url += "health=" + inputHealth.value;
+		isQueryAtLeastOne = true;
+	}
+
+	if (inputDamage.value != "") {
+		if (isQueryAtLeastOne == true) {
+			url += "&";
+		}
+		url += "damage=" + inputDamage.value;
+		isQueryAtLeastOne = true;
+	}
+
+	if (selectClassification.value != "") {
+		if (isQueryAtLeastOne == true) {
+			url += "&";
+		}
+		url += "classification=" + selectClassification.value;
+		isQueryAtLeastOne = true;
+	}
+
+	if (selectType.value != "") {
+		if (isQueryAtLeastOne == true) {
+			url += "&";
+		}
+		url += "type=" + selectType.value;
+		isQueryAtLeastOne = true;
+	}
+
+	if (inputArmor.value != "") {
+		if (isQueryAtLeastOne == true) {
+			url += "&";
+		}
+		url += "armor=" + inputArmor.value;
+		isQueryAtLeastOne = true;
+	}
+
+	console.log(url);
+
+	const response = await fetch(url);
 	const data = await response.json();
+
+	// if (response == []) {
+	// 	console.log("gg bg ta rater");
+	// }
 
 	let divglobalcard = document.createElement("div");
 	divglobalcard.classList.add("div-global-card");
